591f1952b5f289f2ff7b05d7240045df139f4b0d
Glitch (wooded-tabby-pawpaw) <none>
2021-09-10 21:50:18 +0000

    🎰🐛 Checkpoint ./package.json:32646424/1619 ./controllers/dashboard.js:32646424/2834 ./views/layouts/main.hbs:32646424/108

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -4,7 +4,8 @@ const logger = require("../utils/logger");
 const stationControl = require("../models/stationControl.js");
 const uuid = require("uuid");
 const accounts = require("./accounts.js");
-
+const axios = require("axios");
+const oneCallRequest = `https://api.openweathermap.org/data/2.5/onecall?lat=52.160858&lon=-7.152420&units=metric&appid=b425f2808859b7f10ed16f168702a1a5`
 const dashboard = {
   index(request, response) {
     logger.info("dashboard rendering");
@@ -17,11 +18,35 @@ const dashboard = {
         station.lastReading = station.readings[station.readings.length - 1];
       }
     }
+    
     const viewData = {
       title: "Weather Top",
       station: stations
     };
     logger.info("about to render", stationControl.getAllStations());
+    response.render("dashboard", viewData);
+  },
+  
+   async addreport(request, response) {
+    logger.info("rendering new report");
+    let report = {};
+    const lat = request.body.lat;
+    const lng = request.body.lng;
+    const requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&units=metric&appid=YOUR_API_KEY_HERE`
+    const result = await axios.get(requestUrl);
+    if (result.status == 200) {
+      const reading = result.data.current;
+      report.code = reading.weather[0].id;
+      report.temperature = reading.temp;
+      report.windSpeed = reading.wind_speed;
+      report.pressure = reading.pressure;
+      report.windDirection = reading.wind_deg;
+    }
+    console.log(report);
+    const viewData = {
+      title: "Weather Report",
+      reading: report
+    };
     response.render("dashboard", viewData);
   },
 
